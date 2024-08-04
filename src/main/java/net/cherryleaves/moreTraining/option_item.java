ce0908eff9127937286ba82683bc5b75403c6d90
sakurabajp <sakuraba8050@gmail.com>
2024-08-05 00:25:33 +0900

    メニューの下漬け

--- src/main/java/net/cherryleaves/moreTraining/option_item.java
+++ src/main/java/net/cherryleaves/moreTraining/option_item.java
@@ -3,8 +3,10 @@ package net.cherryleaves.moreTraining;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -33,18 +35,51 @@ public class option_item implements Listener {
     }
 
     Inventory menu1 = Bukkit.createInventory(null, 45, ChatColor.DARK_AQUA + "メニュー <詳しくは各アイテムクリック>");
+    String SoundMenuName_on = "サウンド " + ChatColor.GREEN + "on";
+    String SoundMenuName_off = "サウンド " + ChatColor.RED + "off";
+    String TextMenuName_on = "チャット " + ChatColor.GREEN + "on";
+    String TextMenuName_off = "チャット " + ChatColor.RED + "off";
 
     @EventHandler
     public void InventoryClick(InventoryClickEvent e){
         @Nullable ItemStack i = e.getCurrentItem();
+        Player p = (Player) e.getWhoClicked();
         if (i == null) {
             return;
         }
         if(Objects.requireNonNull(i).getType().equals(Material.NETHER_STAR) && i.getItemMeta().getDisplayName().equals("メニュー")){
             e.setCancelled(true);
         }
-        else if(e.getInventory().equals(menu1)){
+        else if(e.getInventory().equals(menu1)) {
             e.setCancelled(true);
+            if(i.getType().equals(Material.JUKEBOX)) {
+                p.playSound(p.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
+                if(i.getItemMeta().getDisplayName().equals(SoundMenuName_on)){
+                    menu1.clear(30);
+                    menu1.setItem(30, setItemDisplayName(Material.JUKEBOX, SoundMenuName_off, new ArrayList<>(List.of("スコアが加算された時にサウンドを鳴らす"))));
+                }
+                if(i.getItemMeta().getDisplayName().equals(SoundMenuName_off)){
+                    menu1.clear(30);
+                    menu1.setItem(30, setItemDisplayName(Material.JUKEBOX, SoundMenuName_on, new ArrayList<>(List.of("スコアが加算された時にサウンドを鳴らす"))));
+                }
+                else{
+                    // ここに後でタグ付ける
+                }
+            }
+            if(i.getType().equals(Material.MAP)){
+                p.playSound(p.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
+                if(i.getItemMeta().getDisplayName().equals(TextMenuName_on)) {
+                    menu1.clear(32);
+                    menu1.setItem(32, setItemDisplayName(Material.MAP, TextMenuName_off, new ArrayList<>(List.of("スコアが加算された時にチャットを送信する"))));
+                }
+                if(i.getItemMeta().getDisplayName().equals(TextMenuName_off)) {
+                    menu1.clear(32);
+                    menu1.setItem(32, setItemDisplayName(Material.MAP, TextMenuName_on, new ArrayList<>(List.of("スコアが加算された時にチャットを送信する"))));
+                }
+                else{
+                    // ここに後でタグ付ける
+                }
+            }
         }
     }
 
